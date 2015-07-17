cd246e969b17494a27aab02ba04355163bec32f8
Tashi Daw <tashiwad@gmail.com>
2015-07-17 20:24:17 +0600

    delete php api and added the rss feed code
    delete php api and added the rss feed code

--- www/php-api/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-	/* Attempt MySQL server connection */
-	$link = mysqli_connect("localhost", "root", "root", "youth_db");
-	 
-	// Check connection
-	if($link === false){
-	    die("ERROR: Could not connect. " . mysqli_connect_error());
-	}
-?>
